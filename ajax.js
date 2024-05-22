bb04ba871ca705da052c8475f0c1a8da6e6a9ce4
Ismail irfan <98257637+Ismailirfan@users.noreply.github.com>
2024-05-22 11:08:09 +0500

    Update ajax.js
    Signed-off-by: Ismail irfan <98257637+Ismailirfan@users.noreply.github.com>

--- ajax.js
+++ ajax.js
@@ -1,7 +1,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 $(document).ready(function(){
   $("button").click(function(){
-    $.ajax({url: "https://www.google.co.uk/", success: function(result){
+    $.ajax({url: "shortcutkeys.html", success: function(result){
       $("#div1").html(result);
     }});
   });
