c825f41d351f9c7448376a8e9260d73d97c39558
Ismail irfan <98257637+Ismailirfan@users.noreply.github.com>
2024-05-22 11:17:27 +0500

    Update ajax.js
    Signed-off-by: Ismail irfan <98257637+Ismailirfan@users.noreply.github.com>

--- ajax.js
+++ ajax.js
@@ -1,6 +1,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 $(document).ready(function(){
-  $("button").click(function(){
+  $("#button").click(function(){
     $.ajax({url: "shortcutkeys.html", success: function(result){
       $("#div1").html(result);
     }});
