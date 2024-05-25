ab606efaf360693d076259b18290ca113bbd285d
Ismail irfan <98257637+Ismailirfan@users.noreply.github.com>
2024-05-25 16:18:07 +0500

    Update ajax.js
    Signed-off-by: Ismail irfan <98257637+Ismailirfan@users.noreply.github.com>

--- ajax.js
+++ ajax.js
@@ -1,8 +1,36 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-$(document).ready(function(){
-  $("#button").click(function(){
-    $.ajax({url: "shortcutkeys.html", success: function(result){
-      $("#div1").html(result);
-    }});
-  });
-});
+   $(document).ready(function() {
+      const API_KEY = 'AIzaSyAAVEyh_nSUsyhpZq-N1oeb1qGNanbz_Kc';
+      const FOLDER_ID = '1_iCnZEDJpQEK524YzL3v8RWUtM9sDqge'; // Replace with your public Google Drive folder ID
+
+      listFiles(); // Automatically fetch files when the page loads
+
+      function listFiles() {
+        $.ajax({
+          url: `https://www.googleapis.com/drive/v3/files?q='${FOLDER_ID}'+in+parents+and+mimeType='text/html'&key=${API_KEY}`,
+          method: 'GET',
+          success: function(data) {
+            $('#fileList').empty();
+            data.files.forEach(function(file) {
+              fetchFileContent(file.id);
+            });
+          },
+          error: function(error) {
+            console.error('Error:', error);
+          }
+        });
+      }
+
+      window.fetchFileContent = function(fileId) {
+        $.ajax({
+          url: `https://www.googleapis.com/drive/v3/files/${fileId}?alt=media&key=${API_KEY}`,
+          method: 'GET',
+          success: function(data) {
+            $('#fileContent').html(data);
+            $('#mymodal').modal('show'); // Show the modal with fetched content
+          },
+          error: function(error) {
+            console.error('Error:', error);
+          }
+        });
+      }
+    });
